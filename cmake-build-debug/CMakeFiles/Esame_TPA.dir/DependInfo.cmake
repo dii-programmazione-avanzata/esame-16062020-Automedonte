92a49d6185814f10d330bddc923dd334393e1e8f
Automedonte <61504444+Automedonte@users.noreply.github.com>
2020-06-16 13:27:42 +0200

    Versione finale

---
+++ cmake-build-debug/CMakeFiles/Esame_TPA.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/main.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/main.cpp.o"
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/src/aula.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/src/aula.cpp.o"
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/src/corso.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/src/corso.cpp.o"
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/src/docente.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/src/docente.cpp.o"
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/src/iscritto.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/src/iscritto.cpp.o"
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/src/lezione.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/src/lezione.cpp.o"
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/src/percorso.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/src/percorso.cpp.o"
+  "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/src/studente.cpp" "/Users/marcello/Documents/GitHub/esame-16062020-Automedonte/cmake-build-debug/CMakeFiles/Esame_TPA.dir/src/studente.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Esame_TPA.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Esame_TPA.dir/main.cpp.o"
+  "CMakeFiles/Esame_TPA.dir/src/aula.cpp.o"
+  "CMakeFiles/Esame_TPA.dir/src/corso.cpp.o"
+  "CMakeFiles/Esame_TPA.dir/src/docente.cpp.o"
+  "CMakeFiles/Esame_TPA.dir/src/iscritto.cpp.o"
+  "CMakeFiles/Esame_TPA.dir/src/lezione.cpp.o"
+  "CMakeFiles/Esame_TPA.dir/src/percorso.cpp.o"
+  "CMakeFiles/Esame_TPA.dir/src/studente.cpp.o"
+  "Esame_TPA"
+  "Esame_TPA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Esame_TPA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
